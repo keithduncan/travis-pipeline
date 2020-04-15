ed666f11858157a57eff2754116e0e3384238fff
Keith Duncan <keith_duncan@me.com>
2020-04-15 11:17:27 +1000

    Add a travis::Rust enum

--- src/travis.rs
+++ src/travis.rs
@@ -12,3 +12,23 @@ pub struct Travis {
 	pub script: Vec<String>,
 	pub matrix: Option<Map<String, Vec<Map<String, String>>>>,
 }
+
+pub enum Rust {
+    Stable,
+    Beta,
+    Nightly,
+    // SemVer release number
+    Release(String),
+}
+
+impl Rust {
+    // Get a docker image for this travis rust tag
+    pub fn image(&self) -> String {
+    	match self {
+    		&Rust::Stable => "rust:latest".to_string(),
+    		&Rust::Beta => unimplemented!(),
+    		&Rust::Nightly => "rustlang/rust:nightly".to_string(),
+    		&Rust::Release(ref ver) => format!("rust:{}", ver),
+    	}
+    }
+}
